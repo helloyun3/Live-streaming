6d90b9cb324a55efa79e01ac1dee02d1583c5360
helloyun3 <13677146417cy>
2019-09-30 22:50:18 +0800

    杨睿帮忙修改了一下，使得能够运行通过。后面会把相关内容补上。

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,2 @@
-INSERT INTO `user` (id, username, password, role) VALUES ('1', 'root', '1234', 'admin');
-INSERT INTO `user` (id, username, password, role) VALUES ('2', 'user1', '1234', 'user');
-INSERT INTO `user` (id, username, password, role) VALUES ('3', 'user2', '1234', 'user');
-INSERT INTO `user` (id, username, password, role) VALUES ('4', 'user3', '1234', 'user');
-INSERT INTO `user` (id, username, password, role) VALUES ('5', 'user4', '1234', 'user');
-commit;
+INSERT INTO TEST VALUES(1, 'Hello');
+INSERT INTO TEST VALUES(2, 'World');
